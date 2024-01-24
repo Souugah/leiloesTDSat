e52d01ba20c49446e249b3154433e7cc19a11866
Gabriela Souza <155557254+Souugah@users.noreply.github.com>
2024-01-24 13:57:16 -0300

    botao de cadastrar produto funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,7 +12,10 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+    
 
 
 public class ProdutosDAO {
@@ -21,14 +24,36 @@ public class ProdutosDAO {
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    conectaDAO conecta;
+    
+    
+     public ProdutosDAO(){
+        this.conecta = new conectaDAO();
+        this.conn = this.conecta.connectDB();
+    
+    }
     
     public void cadastrarProduto (ProdutosDTO produto){
         
         
-        //conn = new conectaDAO().connectDB();
+        try {
+       
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+        
+       PreparedStatement prep = this.conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+        prep.executeUpdate();
+        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+    } catch (SQLException ex) {
+        JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + ex.getMessage());
+    } finally {
+            }
+}                 
         
         
-    }
+    
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -22,7 +22,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=amandameuamor");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
