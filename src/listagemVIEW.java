3c40d8f3c1c1a08658a8d9d7de4ced86ae64f04e
Gabriela Souza <155557254+Souugah@users.noreply.github.com>
2024-01-24 21:21:15 -0300

    botao vender funcionando

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -140,7 +140,7 @@ public class listagemVIEW extends javax.swing.JFrame {
         
         ProdutosDAO produtosdao = new ProdutosDAO();
         
-        //produtosdao.venderProduto(Integer.parseInt(id));
+        produtosdao.venderProduto(Integer.parseInt(id));
         listarProdutos();
     }//GEN-LAST:event_btnVenderActionPerformed
 
